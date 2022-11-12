bffa78b10b955253a5d181a46d6d07204d5b9303
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-12 17:26:49 -0600

    lol

--- index.js
+++ index.js
@@ -28,8 +28,16 @@ const part = function(name, desp, puest, age, net){
   var error="ERROR_CONNECT_BD"
   var retencion=0
 
-  var name_only_chars=""
+  var name_regex=/^[a-zñ\s]{4,255}$/
+  var desp_regex=/a|b\d\d\d/
 
+  if(name!=""){
+    if(name_regex.test(name)){
+
+    }else{
+      error="MAL_NOMBRE"
+    }
+  }
 
   return {error:error,
     retencion:retencion}
@@ -42,7 +50,11 @@ app.get("/particion", async (req, res, next) => {
   var age=decodeURI(req.query.age)
   var net=decodeURI(req.query.net)
 
+  console.log(name, desp, puest, age, net, "request")
+
   var resp = part(name, desp, puest, age, net)
+
+  console.log(resp)
 
   res.json({data:resp,
     msg:"egg"});
