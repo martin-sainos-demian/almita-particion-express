fc498cb4163e1b87c7cee67aa81ea0d6a219f9eb
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-12 16:56:46 -0600

    part func

--- index.js
+++ index.js
@@ -21,8 +21,29 @@ app.all("/", function (req, res, next) {
 app.get("/", async (req, res, next) => {
   res.json({msg:"welcome :3"});
 });
+
+//particion
+
+const part = function(name, desp, puest, age, net){
+  var error="ERROR_CONNECT_BD"
+  var retencion=0
+
+
+  return {error:valid,
+    retencion:retencion}
+}
+
 app.get("/particion", async (req, res, next) => {
-  res.json({msg:"egg"});
+  var name=window.decodeURI(req.query.name)
+  var desp=window.decodeURI(req.query.desp)
+  var puest=window.decodeURI(req.query.puest)
+  var age=window.decodeURI(req.query.age)
+  var net=window.decodeURI(req.query.net)
+
+  var resp = part(name, desp, puest, age, net)
+
+  res.json({data:resp,
+    msg:"egg"});
 });
 //run this sheet
 app.listen(PORT, () => {
